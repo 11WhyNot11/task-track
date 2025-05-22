88d4c531f1c83c249be75799f425ad2f803f6900
11WhyNot11 <arturrebar8@gmail.com>
2025-05-22 12:23:58 +0300

    unify global exception handling and add structured ApiErrorResponse

--- src/main/java/com/arthur/tasktrackerapi/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/arthur/tasktrackerapi/exception/handler/GlobalExceptionHandler.java
@@ -1,52 +1,78 @@
 package com.arthur.tasktrackerapi.exception.handler;
 
+import com.arthur.tasktrackerapi.exception.dto.ApiErrorResponse;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> userNotFoundException(UserNotFoundException exception) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(exception.getMessage());
+    public ResponseEntity<ApiErrorResponse> userNotFoundException(UserNotFoundException exception) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, exception.getMessage());
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<String> userAlreadyExistsException(UserAlreadyExistsException exception) {
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .body(exception.getMessage());
+    public ResponseEntity<ApiErrorResponse> userAlreadyExistsException(UserAlreadyExistsException exception) {
+        return buildErrorResponse(HttpStatus.CONFLICT, exception.getMessage());
     }
 
     @ExceptionHandler(InvalidCredentialsException.class)
-    public ResponseEntity<String> invalidCredentialsException(InvalidCredentialsException exception){
-        return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)
-                .body(exception.getMessage());
+    public ResponseEntity<ApiErrorResponse> invalidCredentialsException(InvalidCredentialsException exception){
+        return buildErrorResponse(HttpStatus.UNAUTHORIZED, exception.getMessage());
     }
 
     @ExceptionHandler(ProjectNotFoundException.class)
-    public  ResponseEntity<String> projectNotFoundException(ProjectNotFoundException exception){
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(exception.getMessage());
+    public  ResponseEntity<ApiErrorResponse> projectNotFoundException(ProjectNotFoundException exception){
+        return buildErrorResponse(HttpStatus.NOT_FOUND, exception.getMessage());
     }
 
     @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity<String> taskNotFoundException(TaskNotFoundException exception) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(exception.getMessage());
+    public ResponseEntity<ApiErrorResponse> taskNotFoundException(TaskNotFoundException exception) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, exception.getMessage());
     }
 
     @ExceptionHandler(CommentNotFoundException.class)
-    public ResponseEntity<String> commentNotFoundException(CommentNotFoundException exception) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(exception.getMessage());
+    public ResponseEntity<ApiErrorResponse> commentNotFoundException(CommentNotFoundException exception) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiErrorResponse> handleAccessDeniedException(AccessDeniedException exception) {
+        return buildErrorResponse(HttpStatus.FORBIDDEN, exception.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleGenericException(Exception exception) {
+        return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong");
+    }
+
+    private ResponseEntity<ApiErrorResponse> buildErrorResponse(HttpStatus status, String message) {
+        ApiErrorResponse error = new ApiErrorResponse(
+                message,
+                status.value(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(error,status);
+
     }
 
 
